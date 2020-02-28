ed219fa750fe7f0522b78105ab67eb081602e1ba
Savitha Peri <savitha.peri@trilio.io>
2020-02-28 14:58:03 +0530

    Update policy.sh

--- policy.sh
+++ policy.sh
@@ -22,4 +22,5 @@ sleep 15s
 mkdir -p /etc/ceph
 cp /etc/kolla/cinder-volume/*.keyring /etc/ceph/
 cp /etc/kolla/cinder-volume/ceph.conf /etc/ceph/
+chmod 644 /etc/ceph/*
 scp /etc/ceph/* root@172.172.3.61:/etc/ceph/
